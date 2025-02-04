b05b7391ffcefbe5857065ff7cf70eb5976ebf6b
Boldchingis <your-email@example.com>
2025-02-04 18:24:54 +0800

    loginPage

--- src/app/_components/Login.tsx
+++ src/app/_components/Login.tsx
@@ -1,11 +1,13 @@
 import { Card } from "@/components/ui/card";
 import LogCard from "./logInput";
+import { Button } from "@/components/ui/button";
 
 export default function Login() {
   return (
-    <div className="flex items-center justify-evenly min-h-screen ">
+    <div className="flex items-center justify-evenly min-h-screen relative">
+      <Button className="absolute top-5 right-5">Sign up</Button>
       <div>
-        <img src="LogPic.png" className=" object-contain" alt="Login" />
+        <img src="LogPic.png" className="object-contain" alt="Login" />
       </div>
       <LogCard />
     </div>
