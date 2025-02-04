e69dabe762abd2ba4618eef6120d482577be1247
24LP8796 24LP8796 <amgaa0329@gmail.com>
2025-02-04 20:08:57 +0800

    2/4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 "use client";
 
 import Image from "next/image";
-import Sign from "./_components/Sign-up";
 import Login from "./_components/Login";
-import CreateProfile from "./CreateProfile";
+
 export default function Home() {
   return (
     <div>
-      <Sign />
       <Login />
-      <CreateProfile />
     </div>
   );
 }
---
+++ src/app/profile/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import Image from "next/image";
+import CreateProfile from "../_components/CreateProfile";
+export default function Signup() {
+  return (
+    <div>
+      <CreateProfile />
+    </div>
+  );
+}
