2f4f9e9fea238a17ced60e1a6d6465290e46d405
amaraa-11 <amaraamygmarjaw@gmail.com>
2025-02-05 14:36:18 +0800

    2/5

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -3,11 +3,11 @@ import { Coffee } from "lucide-react";
 export default function Header() {
   return (
     <div className="flex justify-between mr-10">
-      <div className="flex  text-[#000000] gap-2 font-semibold ml-16 mt-10">
+      <div className="flex  text-black gap-2 font-semibold ml-16 mt-10">
         <Coffee />
         Buy Me Coffee
       </div>
-      <button className="w-16 h-10 rounded-md bg-[#F4F4F5] text-[#18181B] mt-10 mr-16 ">
+      <button className="w-16 h-10 rounded-md bg-gray-200 text-[#18181B] mt-10 mr-16 ">
         Log out
       </button>
     </div>
