220a81734b1d86ed15808535dd7a70cf9a70384b
Boldchingis <your-email@example.com>
2025-02-06 18:07:24 +0800

    sett

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,9 @@
 import Sidebar from "@/app/_components/sidebar";
 
-export default function Dashboard(){
-    return(
-        <div>
-            <Sidebar/>
-        </div>
-    )
-};
+export default function Dashboard() {
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+}
