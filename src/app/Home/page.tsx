978aadcf448e35b5bae43c6ea57d21806abd59cb
24LP8796 24LP8796 <amgaa0329@gmail.com>
2025-02-12 14:41:29 +0800

    1

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import HomePage from "@/app/_components/HomePage";
+import Header from "../_components/header";
 
-export default function HomePageDefault(){
-    return(
-        <div className="flex">
-            <HomePage />
-        </div>
-    )
-};
+export default function HomePageDefault() {
+  return (
+    <div className="flex">
+      <Header />
+      <HomePage />
+    </div>
+  );
+}
