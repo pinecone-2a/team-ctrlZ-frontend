ee7d792e4c5b70d8781647382e1ed21893f94427
REINNn0820 <nymaa4764@gmail.com>
2025-02-12 17:23:34 +0800

    email

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -62,8 +62,6 @@ export default function MultiStepSignup() {
   }
 
   async function handleStepTwo(values: z.infer<typeof stepTwoSchema>) {
-    setIsSubmitting(true);
-
     const mergedData = {
       username: formData.username,
       email: values.email,
@@ -88,9 +86,10 @@ export default function MultiStepSignup() {
       // .then((data) => setData(data));
 
       if (response.ok) {
+        setIsSubmitting(true);
         setTimeout(() => {
           setIsSubmitting(false);
-          router.push("/profile");
+          router.push("/login");
         }, 2000);
       } else {
         console.error("Signup failed");
@@ -245,7 +244,7 @@ export default function MultiStepSignup() {
                   />
                 </FormControl>
                 <FormMessage>
-                  {/* {formStepTwo.formState.errors.email?.message} */}
+                  {formStepTwo.formState.errors.email?.message}
                   {errorMessage}
                 </FormMessage>
               </FormItem>
