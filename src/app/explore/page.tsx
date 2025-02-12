2ce6add8de7c849a451e9989c164dfaaa4ff5e9e
amaraa-11 <amaraamygmarjaw@gmail.com>
2025-02-12 14:25:00 +0800

    update profile

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,13 +1,14 @@
 import Explore from "./explorePage";
 import Sidebar from "../_components/sidebar";
-import Header from "../Home/Header";
+import Header from "../home/Header";
 export default function expPage() {
   return (
-    <div>  
-      <Header/>
-    <div className="flex gap-24">
-      <Sidebar />
-      <Explore />
-    </div></div>
+    <div>
+      <Header />
+      <div className="flex gap-24">
+        <Sidebar />
+        <Explore />
+      </div>
+    </div>
   );
 }
